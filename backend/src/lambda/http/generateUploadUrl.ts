149a0719cef9edb1932031c55cdfcc28178d4233
Kevin Jordan <k.dean.jordan@gmail.com>
2020-06-06 09:32:29 -0700

    feat: req validator added for create and update todo

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,16 +3,13 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 import * as AWS  from 'aws-sdk'
-// import * as uuid from 'uuid'
 
 const imagesBucket = process.env.S3_BUCKET
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION
-// const docClient = new AWS.DynamoDB.DocumentClient()
+
 const s3 = new AWS.S3({
   signatureVersion: 'v4'
 })
-
-
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
